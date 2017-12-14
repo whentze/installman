b866730cab5c487364f9549802586f98562da060
muvlon <muvlon@hentzes.de>
2017-12-14 05:28:14 +0100

    add rudimentary magic-byte matching logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(slice_patterns)]
+
 #[macro_use]
 extern crate lazy_static;
 
@@ -10,9 +12,9 @@ extern crate toml;
 mod config;
 
 
-use std::fs::{self, File, DirBuilder, OpenOptions};
-use std::path::{PathBuf, Path};
-use std::io;
+use std::fs::{self, File};
+use std::path::Path;
+use std::io::{self, Read};
 
 enum TargetType {
     Executable(ExecutableType),
@@ -31,17 +33,28 @@ enum ExecutableType {
 fn classify_target<A: AsRef<Path>>(path: A) -> Result<TargetType, io::Error> {
     use TargetType::*;
     use ExecutableType::*;
-    if fs::metadata(&path)?.is_dir() {
+    let path = path.as_ref();
+    if fs::metadata(path)?.is_dir() {
         return Ok(Directory);
     }
 
-    if let Some(ext) = path.as_ref().extension() {
-        match &*ext.to_string_lossy() {
-            "sh" => { return Ok(Executable(ShellScript)); },
-            "appimage" | "AppImage" => { return Ok(Executable(AppImage)); },
-            _ => {},
-        }
-    }
+    let mut file = File::open(path)?;
+    let mut magic_bytes : [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+    let extension = path.extension().map(|e| e.to_string_lossy().into_owned());
+    file.read_exact(&mut magic_bytes)?;
 
+    match magic_bytes {
+        [0x7F, 0x45, 0x4C, 0x46, ..] => {
+            match extension {
+                Some(ref s) if s == "appimage" || s == "AppImage" => { return Ok(Executable(AppImage)); },
+                _ => { return Ok(Executable(Binary)); },
+            }
+        }
+        [0x1F, 0x8B, ..] | [0x1F, 0x9D, ..] | [0x1F, 0xA0, ..] |
+        [0x42, 0x5A, 0x68, ..] |
+        [0x75, 0x73, 0x74, 0x61, 0x72, 0x00, 0x30, 0x30] |
+        [0x75, 0x73, 0x74, 0x61, 0x72, 0x20, 0x20, 0x00] => { return Ok(Archive); },
+        _ => ()
+    }
     Ok(Unknown)
 }
