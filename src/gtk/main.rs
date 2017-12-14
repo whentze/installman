21b996c501e730082f641ff0744d0e7653aa523b
Jonas Schröder <jschroeder.ac@gmail.com>
2017-12-14 04:36:35 +0100

    added gui.glade and removed main.glade: pre-designed main window using gui.glade in main.rs to open window

--- src/gtk/main.rs
+++ src/gtk/main.rs
@@ -7,28 +7,12 @@ fn main() {
         return;
     }
 
-    let glade_src = include_str!("main.glade");
+    let glade_src = include_str!("gui.glade");
     let builder = gtk::Builder::new_from_string(glade_src);
     let window:  gtk::ApplicationWindow = builder.get_object("main_window").unwrap();
-    let scale:   gtk::Scale             = builder.get_object("goennungs_scale").unwrap();
-    let button:  gtk::CheckButton       = builder.get_object("extra_goennung_button").unwrap();
-    let warning: gtk::Label             = builder.get_object("warnungs_label").unwrap();
-
     window.show_all();
 
-    let scale_clone = scale.clone();
-    let orig_fill_level = scale.get_fill_level();
-    button.connect_toggled(move |but| {
-        if but.get_active() {
-            scale_clone.set_fill_level(std::f64::MAX);
-        } else {
-            scale_clone.set_fill_level(orig_fill_level);
-        }
-    });
-
-    scale.connect_value_changed(move |sca| {
-        warning.set_visible(sca.get_value() > 90.0);
-    });
+    let list_store: gtk::ListStore = builder.get_object("list_store").unwrap();
 
     window.connect_delete_event(move |_, _| {
             gtk::main_quit();
