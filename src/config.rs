08bc35fc37e2b9fcfbfc87af3fc2de3f1bf3ce35
muvlon <muvlon@hentzes.de>
2017-12-14 03:36:35 +0100

    add a couple of default locations

--- src/config.rs
+++ src/config.rs
@@ -7,6 +7,21 @@ lazy_static! {
         path.push(".config/installman/config.toml");
         path
     };
+    pub static ref APPS_LOCATION : PathBuf = {
+        let mut path = env::home_dir().unwrap();
+        path.push("installman_apps");
+        path
+    };
+    pub static ref DESKTOP_FILES_LOCATION : PathBuf = {
+        let mut path = env::home_dir().unwrap();
+        path.push(".local/share/applications");
+        path
+    };
+    pub static ref BIN_SYMLINK_LOCATION : PathBuf = {
+        let mut path = env::home_dir().unwrap();
+        path.push("bin");
+        path
+    };
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -20,20 +35,22 @@ struct AppData {
 #[derive(Serialize, Deserialize, Debug)]
 struct Config {
     apps_location: PathBuf,
+    desktop_files_location: PathBuf,
+    bin_symlink_location: PathBuf,
 }
 
 impl Config {
-    pub fn new<A: AsRef<Path>>(location: A) -> Self {
+    pub fn new<A:AsRef<Path>>(apps: A, desk: A, bins: A) -> Self {
         Config {
-            apps_location : location.as_ref().to_path_buf()
+            apps_location          : apps.as_ref().to_path_buf(),
+            desktop_files_location : desk.as_ref().to_path_buf(),
+            bin_symlink_location   : bins.as_ref().to_path_buf(),
         }
     }
 }
 
 impl Default for Config {
     fn default() -> Self {
-        let mut path = env::home_dir().unwrap();
-        path.push("installman_apps");
-        Config::new(path)
+        Config::new(&*APPS_LOCATION, &*DESKTOP_FILES_LOCATION, &*BIN_SYMLINK_LOCATION)
     }
 }
