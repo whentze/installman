6421b660d3cc3c9eed851f22751445c41355e709
Jonas Schröder <jschroeder.ac@gmail.com>
2017-12-14 06:43:19 +0100

    now a file can be selected in the gui and it answers with the detected TargetType

--- src/gtk/main.rs
+++ src/gtk/main.rs
@@ -1,5 +1,8 @@
 extern crate gtk;
+extern crate installman;
 use gtk::prelude::*;
+use installman::{classify_target, TargetType};
+
 
 fn main() {
     if gtk::init().is_err() {
@@ -9,15 +12,30 @@ fn main() {
 
     let glade_src = include_str!("gui.glade");
     let builder = gtk::Builder::new_from_string(glade_src);
+    let button_install: gtk::Button = builder.get_object("button_install").unwrap();
+    let file_chooser: gtk::FileChooser = builder.get_object("file_chooser").unwrap();
+    let label_file_chooser: gtk::Label = builder.get_object("label_file_chooser").unwrap();
     let window:  gtk::ApplicationWindow = builder.get_object("main_window").unwrap();
     window.show_all();
 
-    let list_store: gtk::ListStore = builder.get_object("list_store").unwrap();
+    //let list_store: gtk::ListStore = builder.get_object("list_store").unwrap();
+
+    button_install.connect_clicked(move|_|{
+        println!("{:?}",file_chooser.get_filename());
+        match file_chooser.get_filename(){
+            Some(x) => match classify_target(x){
+                Ok(TargetType::Executable(_)) => label_file_chooser.set_text("Executable"),
+                Ok(TargetType::Archive) => label_file_chooser.set_text("Archive"),
+                _ => label_file_chooser.set_text("Unknown Type")
+            },
+            None => label_file_chooser.set_text("Please select a file!")
+        }
+
+    });
 
     window.connect_delete_event(move |_, _| {
             gtk::main_quit();
             Inhibit(false)
     });
-
     gtk::main();
 }
