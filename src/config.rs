e38f5200306b08656842276c2fe57290ebf759d7
muvlon <muvlon@hentzes.de>
2017-12-14 03:54:40 +0100

    remove needless `pub`s in config.rs

--- src/config.rs
+++ src/config.rs
@@ -2,22 +2,22 @@ use std::path::{Path, PathBuf};
 use std::env;
 
 lazy_static! {
-    pub static ref CONFIG_LOCATION : PathBuf = {
+    static ref CONFIG_LOCATION : PathBuf = {
         let mut path = env::home_dir().unwrap();
         path.push(".config/installman/config.toml");
         path
     };
-    pub static ref APPS_LOCATION : PathBuf = {
+    static ref APPS_LOCATION : PathBuf = {
         let mut path = env::home_dir().unwrap();
         path.push("installman_apps");
         path
     };
-    pub static ref DESKTOP_FILES_LOCATION : PathBuf = {
+    static ref DESKTOP_FILES_LOCATION : PathBuf = {
         let mut path = env::home_dir().unwrap();
         path.push(".local/share/applications");
         path
     };
-    pub static ref BIN_SYMLINK_LOCATION : PathBuf = {
+    static ref BIN_SYMLINK_LOCATION : PathBuf = {
         let mut path = env::home_dir().unwrap();
         path.push("bin");
         path
@@ -40,7 +40,7 @@ struct Config {
 }
 
 impl Config {
-    pub fn new<A:AsRef<Path>>(apps: A, desk: A, bins: A) -> Self {
+    fn new<A:AsRef<Path>>(apps: A, desk: A, bins: A) -> Self {
         Config {
             apps_location          : apps.as_ref().to_path_buf(),
             desktop_files_location : desk.as_ref().to_path_buf(),
