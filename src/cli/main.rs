604f67a0f7c95bfd34a990750ffd64c88b52c407
muvlon <muvlon@hentzes.de>
2017-12-14 03:54:07 +0100

    add basic CLI commands

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,7 +1,22 @@
 #[macro_use]
 extern crate clap;
+use clap::{Arg, SubCommand};
 
 fn main() {
-    let matches = app_from_crate!().get_matches();
-    println!("dies ist 1 cli");
+    let matches = app_from_crate!()
+        .subcommand(SubCommand::with_name("install")
+            .about("install an app")
+            .arg(Arg::with_name("file")
+                .required(true)
+                .index(1)
+                .help("file to install from")))
+        .subcommand(SubCommand::with_name("list")
+                        .about("list installed apps"))
+        .subcommand(SubCommand::with_name("remove")
+                        .about("uninstall an app")
+                        .arg(Arg::with_name("app")
+                                 .required(true)
+                                 .index(1)
+                                 .help("name of the app to remove")))
+        .get_matches();
 }
