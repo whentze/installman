9a7c86c51136e055891f858be24a3076917c5ce6
muvlon <muvlon@hentzes.de>
2018-01-10 00:29:06 +0100

    more config boilerplate

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,19 @@
 use std::path::{Path, PathBuf};
 use std::env;
+use std::fs::File;
+use std::ffi::OsString;
+use std::io::Write;
+use toml;
 
 lazy_static! {
     static ref CONFIG_LOCATION : PathBuf = {
         let mut path = env::home_dir().unwrap();
         path.push(".config/installman/config.toml");
+        path
+    };
+    static ref DATA_LOCATION : PathBuf = {
+        let mut path = env::home_dir().unwrap();
+        path.push(".config/installman/data.toml");
         path
     };
     static ref APPS_LOCATION : PathBuf = {
@@ -25,24 +34,28 @@ lazy_static! {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct App(PathBuf);
+struct App{
+    name: OsString,
+}
 
-#[derive(Serialize, Deserialize, Debug)]
-struct AppData {
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Data {
     installed_apps: Vec<App>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Config {
+pub(crate) struct Config {
     apps_location: PathBuf,
+    data_location: PathBuf,
     desktop_files_location: PathBuf,
     bin_symlink_location: PathBuf,
 }
 
 impl Config {
-    fn new<A: AsRef<Path>>(apps: A, desk: A, bins: A) -> Self {
+    fn new<A: AsRef<Path>>(apps: A, data: A, desk: A, bins: A) -> Self {
         Config {
             apps_location: apps.as_ref().to_path_buf(),
+            data_location: data.as_ref().to_path_buf(),
             desktop_files_location: desk.as_ref().to_path_buf(),
             bin_symlink_location: bins.as_ref().to_path_buf(),
         }
@@ -52,7 +65,14 @@ impl Config {
 impl Default for Config {
     fn default() -> Self {
         Config::new(&*APPS_LOCATION,
+                    &*DATA_LOCATION,
                     &*DESKTOP_FILES_LOCATION,
                     &*BIN_SYMLINK_LOCATION)
     }
+}
+
+fn write_config(conf : &Config) -> Result<(), Box<::std::error::Error>> {
+    let mut f = File::create(&*CONFIG_LOCATION)?;
+    f.write(&*toml::to_vec(conf)?)?;
+    Ok(())
 }
