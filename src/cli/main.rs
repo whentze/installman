1be0474338321dc80cac4bce784b374459c1e547
muvlon <muvlon@hentzes.de>
2018-01-10 02:28:29 +0100

    use init() in cli

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,12 +1,15 @@
+#![feature(use_nested_groups)]
 #[macro_use]
 extern crate clap;
 extern crate installman;
 
-use clap::{Arg, SubCommand};
-use installman::classify_target;
+use clap::{Arg, ArgMatches, SubCommand, AppSettings};
+use installman::{classify_target, error::{err_msg, Result}};
 
 fn main() {
     let matches = app_from_crate!()
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::ColoredHelp)
         .subcommand(SubCommand::with_name("install")
                         .about("install an app")
                         .arg(Arg::with_name("file")
@@ -14,6 +17,7 @@ fn main() {
                                  .index(1)
                                  .help("file to install from")))
         .subcommand(SubCommand::with_name("list").about("list installed apps"))
+        .subcommand(SubCommand::with_name("init").about("initialize files"))
         .subcommand(SubCommand::with_name("remove")
                         .about("uninstall an app")
                         .arg(Arg::with_name("app")
@@ -21,11 +25,25 @@ fn main() {
                                  .index(1)
                                  .help("name of the app to remove")))
         .get_matches();
-    if let Some(matches) = matches.subcommand_matches("install") {
-        let target = matches.value_of("file").unwrap();
-        match classify_target(target){
-            Ok(x) => println!("File identified as: {}", x),
-            Err(_) => println!("Target Classification Failed!"),
-        }
+
+    if let (subcommand, Some(matches)) = matches.subcommand() {
+        run_subcommand(subcommand, matches).unwrap_or_else(|e| {
+            eprintln!("Unhandled error: {}", e);
+            std::process::exit(1);
+        });
     }
+}
+
+fn run_subcommand(subcommand: &str, matches: &ArgMatches) -> Result<()> {
+    match subcommand {
+        "install" => {
+            let target = matches.value_of("file").unwrap();
+            println!("File identified as: {}", classify_target(target)?);
+        },
+        "init" => {
+            installman::init()?
+        },
+        _ => return Err(err_msg("Unknown subcommand")),
+    };
+    Ok(())
 }
